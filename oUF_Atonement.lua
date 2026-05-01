5c34de41b67224d06727788a8f7ffeb0f1fd7bd8
pedrozc90 <pedrozc90@gmail.com>
2026-05-01 18:57:24 -0300

    update aura api

--- oUF_Atonement.lua
+++ oUF_Atonement.lua
@@ -38,132 +38,152 @@ local oUF = ns.oUF or oUF
 assert(oUF, "oUF_Atonement was unable to locate oUF install.")
 
 -- Blizzard
-local GetSpecialization = _G.GetSpecialization
-local UnitClass = _G.UnitClass
-local UnitBuff = _G.UnitBuff
 
 -- Constants
 local SPEC_PRIEST_DISCIPLINE = 1
-local MAX_AURAS = 40
 local ATONEMENT_COLOR = oUF:CreateColor(0.81, 0.71, 0.23)
 
-local auras = {
+local ATONEMENT_SPELLS = {
 	[194384] = true,	-- Atonement
 	[214206] = true		-- Atonement PvP Talent
 }
 
-local function OnUpdate(self, elapsed)
-	self.elapsed = (self.elapsed or 0) + elapsed
-	if (self.elapsed >= 0.1) then
-		local value = (self.expirationTime or 0) - GetTime()
-		if (value > 0) then
-			self:SetValue(value)
+local function UpdateColor(element)
+	local color = element.color
+	if (color) then
+		element:SetStatusBarColor(color.r, color.g, color.b, color.a or 1)
+
+		local bg = element.bg
+		if bg then
+			local mu = bg.multiplier or 1
+			bg:SetVertexColor(color.r * mu, color.g * mu, color.b * mu, (color.a * mu) or 1)
+		end
+	end
+end
+
+local function UpdateAura(element, unit, data)
+	if (data) then
+		local duration = C_UnitAuras.GetAuraDuration(unit, data.auraInstanceID)
+		element.duration = duration
+
+		if (duration) then
+			element:SetTimerDuration(duration, element.interpolation, element.direction)
+			element:Show()
 		else
-			self:SetScript("OnUpdate", nil)
-			self:SetValue(0)
+			element.duration = nil
+			element:SetValue(0)
+			element:Hide()
 		end
-	end
-end
-
-local GetUnitAura
-if C_UnitAuras then
-	GetUnitAura = function(unit, index, filter)
-		return C_UnitAuras.GetBuffDataByIndex(unit, index, filter)
-	end
-else
-	GetUnitAura = function(unit, index, filter)
-		local name, icon, count, dispelType, duration, expirationTime, source, isStealable, nameplateShowPersonal,
-		spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod = UnitBuff(unit, index, filter)
-		if name then
-			return {
-				auraInstanceID = index,
-				name = name,
-				icon = icon,
-				spellId = spellId,
-				sourceUnit = source,
-				dispelName = dispelType,
-				applications = count,
-				canApplyAura = canApplyAura,
-				duration = duration,
-				expirationTime = expirationTime,
-				isStealable = isStealable,
-				isBossAura = isBossDebuff,
-				isFromPlayerOrPlayerPet = castByPlayer,
-				isHarmful = (filter == "HARMFUL"),
-				isHelpful = (filter == "HELPFUL"),
-				isRaid = (filter == "RAID"),
-				isNameplateOnly = false,
-				nameplateShowAll = nameplateShowAll,
-				nameplateShowPersonal = false,
-				charges = nil,
-				maxCharges = nil,
-				points = nil,
-				timeMod = timeMod
-			}
-		end
-	end
-end
-
-local function Update(self, event, unit)
-	if self.unit ~= unit then return end
-	local element = self.Atonement
-
-	if element then
-		for index = 1, MAX_AURAS do
-			local aura = GetUnitAura(unit, index, "HELPFUL")
-			if aura then
-				if auras[aura.spellId] then
-					element.duration = aura.duration or 0
-					element.expirationTime = aura.expirationTime or (GetTime() + element.duration)
-					
-					element:SetMinMaxValues(0, element.duration)
-					element:SetScript("OnUpdate", OnUpdate)
-					element:Show()
-
-					return
-				end
-			end
-		end
-
-		element:SetScript("OnUpdate", nil)
+	else
+		element.duration = nil
 		element:SetValue(0)
 		element:Hide()
 	end
 end
 
+local function FilterAuras(unit, data)
+	return data and ATONEMENT_SPELLS[data.spellId]
+end
+
+local function Update(self, event, unit, updateInfo)
+	if self.unit ~= unit then return end
+	
+	local element = self.Atonement
+	if not element then return end
+
+	local isFullUpdate = not updateInfo or updateInfo.isFullUpdate
+
+	local changed = false
+
+	if (isFullUpdate) then
+		element.data = nil
+
+		local slots = { C_UnitAuras.GetAuraSlots(unit, element.filter) }
+		for i = 2, #slots do
+			local data = C_UnitAuras.GetAuraDataBySlot(unit, slots[i])
+			if FilterAuras(unit, data) then
+				element.data = data
+				break
+			end
+		end
+
+		changed = true
+	else
+		if (updateInfo.addedAuras) then
+			for _, data in next, updateInfo.addedAuras do
+				if FilterAuras(unit, data) then
+					element.data = data
+					changed = true
+					break
+				end
+			end
+		end
+
+		if updateInfo.updatedAuraInstancesIDs then
+			for _, auraInstanceID in next, updateInfo.updatedAuraInstancesIDs do
+				local data = C_UnitAuras.GetAuraDataByInstanceID(unit, auraInstanceID)
+				if FilterAuras(unit, data) then
+					element.data = data
+					changed = true
+					break
+				end
+			end
+		end
+
+		if updateInfo.removedAuraInstancesIDs then
+			for _, auraInstanceID in next, updateInfo.removedAuraInstancesIDs do
+				if (element.data and element.data.auraInstanceID == auraInstanceID) then
+					element.data = nil
+					changed = true
+					break
+				end
+			end
+		end
+	end
+
+	if changed then
+		UpdateAura(element, unit, element.data)
+	elseif (element.data) then
+		local data = C_UnitAuras.GetPlayerAuraBySpellID(element.data.spellId)
+		if (not data or data.auraInstanceID ~= element.data.auraInstanceID) then
+			element.data = data
+			UpdateAura(element, unit, data)
+		end
+	end
+end
+
 local function Visibility(self, event)
+	local element = self.Atonement
+
 	local _, class = UnitClass("player")
-	local specialization = GetSpecialization()
+	local specialization = C_SpecializationInfo.GetSpecialization()
+
+	element:Hide()
 
 	if (class == "PRIEST") and (specialization == SPEC_PRIEST_DISCIPLINE) then
+		UpdateColor(element)
 		self:RegisterEvent("UNIT_AURA", Update)
 	else
-		self.Atonement:SetScript("OnUpdate", nil)
-		self.Atonement:SetValue(0)
-		self.Atonement:Hide()
-
 		self:UnregisterEvent("UNIT_AURA", Update)
 	end
+end
+
+local function ForceUpdate(element)
+	Visibility(element.__owner, "ForceUpdate", element.__owner.unit)
 end
 
 local function Enable(self)
 	local element = self.Atonement
 	if element then
+		element.__owner = self
+		element.ForceUpdate = ForceUpdate
 		element.color = element.color or ATONEMENT_COLOR
+		element.filter = "HELPFUL|PLAYER"
+		element.interpolation = Enum.StatusBarInterpolation.Immediate
+		element.direction = Enum.StatusBarTimerDirection.RemainingTime
 
-		self:RegisterEvent("UNIT_AURA", Update)
 		self:RegisterEvent("PLAYER_TALENT_UPDATE", Visibility, true)
 		self:RegisterEvent("PLAYER_ENTERING_WORLD", Visibility, true)
-
-		local color = element.color
-		element:SetValue(0)
-		element:SetStatusBarColor(color.r, color.g, color.b)
-
-		local bg = element.bg
-		if bg then
-			local mu = bg.multiplier or 1
-			bg:SetVertexColor(color.r * mu, color.g * mu, color.b * mu)
-		end
 
 		element:Hide()
 
